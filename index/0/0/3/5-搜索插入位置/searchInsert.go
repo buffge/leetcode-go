11c9a38e6bba1fd46837b1294d720e0b221088f9
buffge <1515888956@qq.com>
2020-08-28 23:29:27 +0800

    update index-35 搜索插入位置

--- index/0/0/3/5-搜索插入位置/searchInsert.go
+++ index/0/0/3/5-搜索插入位置/searchInsert.go
@@ -20,30 +20,25 @@ func searchInsert(nums []int, target int) int {
 
 /**
 二分法
-取中间值 如果大于目标值 二分左侧,左侧完 返回lo
+取中间值 如果大于等于目标值 二分左侧,左侧完 返回lo
+必须取到最左侧一个等于目标值的才算找到索引,所以大于等于都要向左继续二分
 取中间值 如果小于目标值 二分右侧 右侧完 返回hi
 如果等于 返回idx
 */
 func searchInsertV2(nums []int, target int) int {
-	nLen := len(nums)
-	lo, hi, mid := 0, nLen-1, nLen>>1
-	for mid >= 0 {
-		log.Println(lo, hi, mid)
-		if mid == lo {
-			return lo
-		}
-		if nums[mid] > target {
-			hi = mid
-			mid = lo + (hi-lo)>>1
+	lo, hi := 0, len(nums)-1
+	for lo <= hi {
+		mid := lo + (hi-lo)>>1
+		if nums[mid] >= target {
+			hi = mid - 1
 		} else {
-			lo = mid
-			mid = lo + (hi-lo)>>1
+			lo = mid + 1
 		}
 	}
-	return 0
+	return lo
 }
 
 func main() {
 	log.Println(searchInsert([]int{1, 3, 5, 6}, 58))
-	log.Println(searchInsertV2([]int{1, 3, 5, 6}, 58))
+	log.Println(searchInsertV2([]int{1, 3, 5, 6}, 3))
 }

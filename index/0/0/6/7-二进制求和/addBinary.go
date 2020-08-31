b25a8442e58e1ce3a9094b18b117c5ab8b33f950
buffge <1515888956@qq.com>
2020-08-31 21:50:10 +0800

    update index-67 二进制求和

--- index/0/0/6/7-二进制求和/addBinary.go
+++ index/0/0/6/7-二进制求和/addBinary.go
@@ -2,6 +2,14 @@ package main
 
 import "log"
 
+/**
+思路:
+计算出最大长度,
+开辟数组,长度为maxLen+1,用来存放二进制数
+数组首位用来进位
+逆序遍历,如果a,b 此位存在则相加
+如果需要进位则加1,如果>=2 则-2 进位
+*/
 func addBinary(a string, b string) string {
 	aLen := len(a)
 	bLen := len(b)
@@ -11,31 +19,32 @@ func addBinary(a string, b string) string {
 	} else {
 		maxLen = bLen
 	}
-	arr := make([]byte, maxLen)
-	arr[0] = '0'
+	arr := make([]byte, maxLen+1)
 	carry := false
-	for i := maxLen - 1; i >= 0; i-- {
+	for i := maxLen; i >= 0; i-- {
 		if carry {
-			arr[i] += '1'
+			carry = false
+			arr[i]++
 		}
-		if i >= maxLen-aLen {
-			arr[i] += a[i-maxLen+aLen]
+		arr[i] += '0'
+		if i >= maxLen-aLen+1 {
+			arr[i] += (a[i-maxLen+aLen-1] - '0')
 		}
-		if i >= maxLen-bLen {
-			arr[i] += b[i-maxLen+bLen]
+		if i >= maxLen-bLen+1 {
+			arr[i] += (b[i-maxLen+bLen-1] - '0')
 		}
-		if arr[i] > '1' {
+		if arr[i] >= '2' {
 			carry = true
-			arr[i] -= '2'
+			arr[i] -= 2
 		}
 	}
 	if arr[0] == '0' {
 		arr = arr[1:]
 	}
-	log.Println(arr)
 	return string(arr)
 }
+
 func main() {
-	log.Println(addBinary("11", "1"))
+	log.Println(addBinary("1010", "1011"))
 	log.Println(addBinary("1011", "11"))
 }

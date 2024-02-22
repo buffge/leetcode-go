333e2170decdd6f36330dbb3a7e4537f4e3c50c5
buffge <admin@buffge.com>
2024-02-22 17:59:41 +0800

    优化 1. 两数之和

--- index/0/0/0/1-twoSum/twoSum.go
+++ index/0/0/0/1-twoSum/twoSum.go
@@ -1,47 +1,20 @@
-package leetcode
+package main
 
-/**
-给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
+import "log"
 
-你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
-
-示例:
-
-给定 nums = [2, 7, 11, 15], target = 9
-
-因为 nums[0] + nums[1] = 2 + 7 = 9
-所以返回 [0, 1]
-
-链接：https://leetcode-cn.com/problems/two-sum
-*/
+/* 1. 两数之和
+ * 思路 用一个hash存val对应的idx 遍历数组找到 target-currVal的索引
+ */
 func twoSum(nums []int, target int) []int {
-	m := make(map[int]int)
-	for k, v := range nums {
-		m[v] = k
-	}
-	arrLen := len(nums)
-	if arrLen < 2 {
-		panic("数组长度必须大于1")
-	}
-	for i := 0; i < arrLen-1; i++ {
-		if k, ok := m[target-nums[i]]; ok && k != i {
-			return []int{i, k}
+	m := make(map[int]int, len(nums))
+	for i, num := range nums {
+		if res, exist := m[target-num]; exist {
+			return []int{res, i}
 		}
+		m[num] = i
 	}
-	return []int{}
+	return nil
 }
-func twoSumV2(nums []int, target int) []int {
-	m := make(map[int]int)
-	arrLen := len(nums)
-	if arrLen < 2 {
-		panic("数组长度必须大于1")
-	}
-	for k, v := range nums {
-		idx, ok := m[target-v]
-		if ok {
-			return []int{k, idx}
-		}
-		m[v] = k
-	}
-	return []int{}
+func main() {
+	log.Println(twoSum([]int{2, 7, 11, 15}, 9))
 }

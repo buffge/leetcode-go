42b7c1a71d19593bf6c7e2df9490fc6818c3c494
buffge <admin@buffge.com>
2024-08-06 03:50:27 +0800

    默写 101.对称二叉树

--- index/0/1/0/1-101/101.go
+++ index/0/1/0/1-101/101.go
@@ -24,7 +24,7 @@ func check(left, right *TreeNode) bool {
 }
 
 // 是否为对称二叉树 递归版
-func isSymmetric(root *TreeNode) bool {
+func isSymmetricV1(root *TreeNode) bool {
 	if root == nil {
 		return true
 	}
@@ -51,6 +51,32 @@ func isSymmetricV2(root *TreeNode) bool {
 	}
 	return true
 }
+
+/*
+默写
+*/
+func isSymmetricExam1(root *TreeNode) bool {
+	if root == nil {
+		return false
+	}
+	stack := []*TreeNode{root.Left, root.Right}
+	for len(stack) > 0 {
+		right := stack[len(stack)-1]
+		left := stack[len(stack)-2]
+		stack = stack[:len(stack)-2]
+		if left == nil && right == nil {
+			continue
+		}
+		if left == nil || right == nil || left.Val != right.Val {
+			return false
+		}
+		stack = append(stack, left.Left, right.Right, left.Right, right.Left)
+	}
+	return true
+}
+
+var isSymmetric = isSymmetricExam1
+
 func main() {
 	t1 := newTree(1)
 	t1.Left = newTree(2)

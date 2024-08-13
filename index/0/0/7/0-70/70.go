d72315b05b7a9feaa89c72b2384d4ac1bdf6d9e4
buffge <admin@buffge.com>
2024-08-13 22:06:58 +0800

    复习 70. 爬楼梯

--- index/0/0/7/0-70/70.go
+++ index/0/0/7/0-70/70.go
@@ -2,15 +2,14 @@ package main
 
 import "log"
 
-/**
+/*
+*
 思路: f(n) = f(n-1) + f(n-2)
 因为在 爬到第n层前 要么在第n-1层 要么在n-2层
 n-1 爬 2 到第n层  = f(n-1) 就把f(n-1)所有的尾节点后面加个->2
 n-2 爬 1 到第n层  = f(n-2) 就把f(n-2)所有的尾节点后面加个->1
-
-
 */
-func climbStairs(n int) int {
+func climbStairsV1(n int) int {
 	dp := [50]int{1, 2}
 	if n <= 2 {
 		return dp[n-1]
@@ -28,6 +27,24 @@ func climbStairsV2(n int) int {
 	}
 	return z
 }
+
+/*
+*
+n(2) = 2
+n(3) = 2 + 1
+爬 n = (n-1) + 1 | (n-2) + 2
+*/
+func climbStairsS1(n int) int {
+	x, y, z := 0, 0, 1 // n-2 ,n-1,n
+	for i := 1; i <= n; i++ {
+		x, y = y, z // n-2,n-1 = n-1,n
+		z = x + y   // n  = n-1 + n-2
+	}
+	return z
+}
+
+var climbStairs = climbStairsS1
+
 func main() {
 	log.Println(climbStairs(3))
 	log.Println(climbStairs(8))
